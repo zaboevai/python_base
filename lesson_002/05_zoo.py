94b1fa8ec23d732c66069a4c9e0b057ced763bb9
aizab <aizab@bank-hlynov.ru>
2018-12-29 08:31:30 +0300

    les2, 05 - Переместил животных в зоопарке

--- lesson_002/05_zoo.py
+++ lesson_002/05_zoo.py
@@ -7,19 +7,23 @@ zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
 
 # посадите медведя (bear) между львом и кенгуру
 #  и выведите список на консоль
-# TODO здесь ваш код
+zoo.insert(2,'bear')
+print(zoo)
 
 # добавьте птиц из списка birds в последние клетки зоопарка
 birds = ['rooster', 'ostrich', 'lark', ]
 #  и выведите список на консоль
-# TODO здесь ваш код
+zoo.extend(birds)
+print(zoo)
 
 # уберите слона
 #  и выведите список на консоль
-# TODO здесь ваш код
+zoo.remove('elephant')
+print(zoo)
 
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
-# TODO здесь ваш код
+print(zoo.index('lion')+1)
+print(zoo.index('lark')+1)
 
 
