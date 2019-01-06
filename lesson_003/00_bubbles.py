e04939bfd2a865f953d8f490e73d566f5be89596
zaboevai <zaboevai@yandex.ru>
2019-01-06 10:50:04 +0300

    les3, 00 - Нарисовал круги с использованием циклов

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -1,23 +1,47 @@
 # -*- coding: utf-8 -*-
+import random
+import time
 
 import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
+def draw_circle(position , radius=50, step = 0, color=sd.COLOR_GREEN, width=2):
+    sd.circle(center_position=position, radius=radius+step, width=width, color=color)
+
+
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
+step = 0
+for y in range(3):
+    circle_position = sd.get_point(600, 300)
+    sd.circle(center_position=circle_position, radius=100+step, width=1)
+    step += 5
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO здесь ваш код
+
+circle_position = sd.get_point(600, 300)
+draw_circle(position=circle_position, radius=50, color=sd.COLOR_DARK_RED, width=10)
 
 # Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
 
-# Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+for x in range(100, 1001, 100):
+    circle_position = sd.get_point(x, 500)
+    draw_circle(position=circle_position, radius=50, color=sd.COLOR_DARK_RED)
 
-# Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-# TODO здесь ваш код
+# # Нарисовать три ряда по 10 пузырьков
+
+for y in range(100, 301, 100):
+    for x in range(100, 1001, 100):
+        circle_position = sd.get_point(x, y)
+        draw_circle(position=circle_position, radius=50)
+
+
+# # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
+for _ in range(100):
+    circle_position = sd.random_point()
+    step = random.randint(1, 100)
+    time.sleep(0.1)
+    draw_circle(position=circle_position, radius=50, step=step, color=sd.random_color(), width=0)
 
 sd.pause()
 
