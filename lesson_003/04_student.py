adf0821ce95584ac985a75ec5400199f04e7a0ce
zaboevai <zaboevai@yandex.ru>
2019-01-09 19:11:42 +0300

    les3, 04 - Подсчитал сколько требуется д/ср для выживания студента

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -10,5 +10,17 @@
 #   Студенту надо попросить ХХХ.ХХ рублей
 
 educational_grant, expenses = 10000, 12000
+i = 0
+student_income = 0
+months_expenses = 0
 
-# TODO здесь ваш код
+while i < 10:
+    student_income += educational_grant
+    if i == 0:
+        months_expenses += expenses
+    elif i >= 1:
+        months_expenses += expenses + expenses * 0.03
+    i += 1
+    # print(months_expenses, student_income, months_expenses-student_income)
+
+print('Студенту надо попросить', months_expenses-student_income, 'рублей')
