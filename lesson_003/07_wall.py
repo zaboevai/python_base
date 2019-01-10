7db9a9f5f6bf19d3cec05dafd8a9c4635b40105f
aizab <aizab@bank-hlynov.ru>
2019-01-10 16:05:18 +0300

    les3, 07 - Кирпичная стена строится, но есть ошибки и код требует оптимизации

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -12,8 +12,8 @@ sd.background_color = sd.COLOR_DARK_ORANGE
 x = 100
 y = 50
 
-pos_x = 0
-pos_y = 0
+pos_x = 50
+pos_y = 50
 # start_x = 50
 # end_x = start_x + x
 #
@@ -26,6 +26,8 @@ step = x
 brick_x_count = sd.resolution[0] // x
 brick_y_count = sd.resolution[1] // y
 
+x_count = brick_x_count
+
 sd.resolution = (800, 800)
 
 start_x = pos_x
@@ -34,25 +36,45 @@ end_y = start_y + y
 end_x = start_x + x
 
 for brick_y in range(brick_y_count):
-    start_x = pos_x
-    start_y = pos_y
-    for brick_x in range(brick_x_count):
 
-        start_position = sd.get_point(start_x, start_y)
-        end_position = sd.get_point(end_x, end_y)
-        sd.rectangle(left_bottom=start_position, right_top=end_position, color=sd.COLOR_BLACK, width=1)
-        start_x += 100 - 1
-        # start_y += 50
-        end_x += 100 - 1
+    if brick_y % 2 == 1:
+        x_count += 1
+    else:
+        x_count = brick_x_count #sd.resolution[0] // x
+
+    for brick_x in range(x_count):
+
         # print(brick_y, brick_y % 2)
         if brick_y % 2 == 1:
-            step = 50
+            print(brick_x, x_count)
+            if brick_x == 0: # or brick_x == x_count:
+                start_position = sd.get_point(start_x, start_y)
+                end_position = sd.get_point(end_x-50, end_y)
+            elif brick_x == x_count-1:
+                start_position = sd.get_point(start_x-50, start_y)
+                end_position = sd.get_point(end_x-100, end_y)
+            else:
+                start_position = sd.get_point(start_x-50, start_y)
+                end_position = sd.get_point(end_x-50, end_y)
         else:
-            step = 0
+            start_position = sd.get_point(start_x, start_y)
+            end_position = sd.get_point(end_x, end_y)
 
+
+        sd.rectangle(left_bottom=start_position, right_top=end_position, color=sd.COLOR_BLACK, width=1)
+
+        # двигаем кирпич по x
+        start_x += pos_x + x
+        end_x += x
+
+        sd.sleep(0.03)
+
+    # возвращаем по х в исходное положение
     start_x = pos_x
-    start_y = pos_y
-    end_x = pos_x + x
+    end_x = x
+
+    # двигаем кирпич по y
+    start_y = end_y
     end_y += y
 
 sd.pause()
