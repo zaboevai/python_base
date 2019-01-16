98ecfdaffa7a7fed387e0c70df3dddc41f529e7b
zaboevai <zaboevai@yandex.ru>
2019-01-16 23:46:35 +0300

    les4, 04 - Построил дерево с помощью рекурсии + частичное выполнение усложненного задания

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -26,7 +26,28 @@ import simple_draw as sd
 
 # можно поиграть -шрифтами- цветами и углами отклонения
 
-# TODO здесь ваш код
+sd.resolution=(1200, 800)
+def draw_branches(point, angle, length=100):
+    if length < 10:
+        return
+
+    vector = sd.get_vector(start_point=point, angle=angle, length=length, width=2)
+    vector.draw()
+
+    next_point = vector.end_point
+
+    delta = 30
+    next_length = length * 0.75
+
+    next_angle = angle - delta
+    draw_branches(next_point, next_angle, next_length)
+
+    next_angle = angle + delta
+    draw_branches(next_point, next_angle, next_length)
+
+
+root_point = sd.get_point(900, 30)
+draw_branches(point=root_point, angle=90, length=100)
 
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
@@ -35,6 +56,40 @@ import simple_draw as sd
 
 # Пригодятся функции
 # sd.random_number()
+
+sd.resolution=(1200, 800)
+def draw_branches(point, angle, length=100):
+    if length < 10:
+        return
+
+    vector = sd.get_vector(start_point=point, angle=angle, length=length, width=2)
+    vector.draw()
+
+    next_point = vector.end_point
+
+    delta = 30
+    delta_deviation = delta * 0.4
+    delta_deviation = delta + sd.randint(-delta_deviation, delta_deviation)
+    next_angle = round(angle - delta_deviation)
+
+    next_length = length * 0.8
+
+    # length_deviation = round(next_length * 0.2)
+    # next_length += length_deviation + sd.randint(-length_deviation, length_deviation)
+    # print(next_length)
+    # next_length += length_deviation
+
+    draw_branches(next_point, next_angle, next_length)
+
+    delta_deviation = delta + sd.randint(-delta_deviation, delta_deviation)
+    next_angle = round(angle + delta_deviation)
+    draw_branches(next_point, next_angle, next_length)
+
+
+root_point = sd.get_point(300, 30)
+draw_branches(point=root_point, angle=90, length=100)
+
+
 
 sd.pause()
 
