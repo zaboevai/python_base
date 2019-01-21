693d75fea1771ee05cbbf3849320e1be1c8ff688
zaboevai <zaboevai@yandex.ru>
2019-01-21 17:08:14 +0300

    les5, 02 - Вывел жителей района

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -7,6 +7,22 @@
 
 # TODO здесь ваш код
 
+from district.central_street.house1 import room1 as central_house1_room1, room2 as central_house1_room2
+from district.central_street.house2 import room1 as central_house2_room1, room2 as central_house2_room2
+from district.soviet_street.house1 import room1 as soviet_house1_room1, room2 as soviet_house1_room2
+from district.soviet_street.house2 import room1 as soviet_house2_room1, room2 as soviet_house2_room2
 
 
+inhabitant = ', '
+my_list = []
+my_list.extend(central_house1_room1.folks)
+my_list.extend(central_house1_room2.folks)
+my_list.extend(central_house2_room1.folks)
+my_list.extend(central_house2_room2.folks)
 
+my_list.extend(soviet_house1_room1.folks)
+my_list.extend(soviet_house1_room2.folks)
+my_list.extend(soviet_house2_room1.folks)
+my_list.extend(soviet_house2_room2.folks)
+
+print(inhabitant.join(my_list))
