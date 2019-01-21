0c827af8f981fd28a578dcd1ccf017bc5bc99935
zaboevai <zaboevai@yandex.ru>
2019-01-21 17:07:56 +0300

    les5, 01 - Вывел жителей комнат

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -4,5 +4,10 @@
 # Формат: В комнате room_1 живут: ...
 
 # TODO здесь ваш код
+import room_1 as rm1
+import room_2 as rm2
 
-
+inhabitant = ', '.join(rm1.folks)
+print('В комнате room_1 живут:', inhabitant)
+inhabitant = ', '.join(rm2.folks)
+print('В комнате room_2 живут:', inhabitant)
