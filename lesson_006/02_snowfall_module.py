fbf0dbe0fbbc7c26feed03bd76ae8659b9d21233
zaboevai <zaboevai@yandex.ru>
2019-02-06 16:29:45 +0300

    les6, 02 - Создал модуль и клиентскую часть снегопада (не рабочая)

--- lesson_006/02_snowfall_module.py
+++ lesson_006/02_snowfall_module.py
@@ -14,14 +14,38 @@ import simple_draw as sd
 # обращаясь ТОЛЬКО к функциям модуля snowfall
 
 # создать_снежинки(N)
+from snowfall import create_snowflakes, \
+                     draw_snowflakes, \
+                     move_snowflakes, \
+                     remove_snowflakes, \
+                     check_down_snowflakes, \
+                     num_remove_snowflake
+
+snowflakes_count = 5
+create_snowflakes(snowflakes_count)
+
 while True:
+
+    sd.start_drawing()
+    draw_snowflakes(sd.background_color)
+    move_snowflakes()
+    draw_snowflakes(sd.COLOR_WHITE)
+
+    # print(len(num_remove_snowflake))
+    if check_down_snowflakes():
+        # print(len(num_remove_snowflake))
+        remove_snowflakes(num_remove_snowflake)
+        create_snowflakes(len(num_remove_snowflake))
+        num_remove_snowflake = []
+    sd.sleep(0.05)
+    sd.finish_drawing()
+
     #  нарисовать_снежинки_цветом(color=sd.background_color)
     #  сдвинуть_снежинки()
     #  нарисовать_снежинки_цветом(color)
     #  если есть номера_достигших_низа_экрана() то
     #       удалить_снежинки(номера)
-    #       создать_снежинки(count)
-    sd.sleep(0.1)
+    #       создать_снежинки(count)    #
     if sd.user_want_exit():
         break
 
---
+++ lesson_006/snowfall.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+import simple_draw as sd
+
+sd.resolution = (1200, 800)
+
+snowflake_size = {'min': 5, 'max': 20}
+
+_snowflakes = {}
+_down_snowflakes = {}
+num_remove_snowflake = []
+
+
+def create_snowflakes(snowflakes_count=1):
+    global _snowflakes
+
+    new_snowflakes = {}
+    y = 700
+    len_dict = 0
+    print(_snowflakes.keys())
+
+    for key, value in _snowflakes.items():
+        new_snowflakes[len_dict] = value
+        len_dict += 1
+    print(new_snowflakes.keys())
+
+    for i in range(snowflakes_count):
+        new_snowflakes[len_dict + i] = {'length': sd.random_number(snowflake_size['min'], snowflake_size['max']),
+                                        'x': sd.random_number(0, sd.resolution[0]),
+                                        'y': y,
+                                        'factor_a': sd.random_number(1, 10)/10,
+                                        'factor_b': sd.random_number(1, 10)/10,
+                                        'factor_c': sd.random_number(1, 120)
+                                        }
+
+    # TODO Подумать как правильно заполнять словарь
+    print(new_snowflakes.keys())
+
+    _snowflakes.update(new_snowflakes)
+    print(_snowflakes)
+
+
+def remove_snowflakes(num_snowflake):
+    global _snowflakes
+
+    _snowflakes = {k: v for k, v in _snowflakes.items() if k not in num_snowflake}
+
+
+def draw_snowflakes(color=sd.COLOR_WHITE):
+    for snowflake_num, snowflake_parameter in _snowflakes.items():
+        start_point = sd.get_point(snowflake_parameter['x'], snowflake_parameter['y'])
+        sd.snowflake(center=start_point,
+                     length=snowflake_parameter['length'],
+                     color=color,
+                     factor_a=snowflake_parameter['factor_a'],
+                     factor_b=snowflake_parameter['factor_b'],
+                     factor_c=snowflake_parameter['factor_c'])
+
+
+def move_snowflakes():
+    for snowflake_num, snowflake_parameter in _snowflakes.items():
+        snowflake_parameter['x'] += sd.random_number(0, 2)
+        snowflake_parameter['y'] -= snowflake_size['max'] + 1 - snowflake_parameter['length']
+
+
+def check_down_snowflakes():
+    global num_remove_snowflake
+
+    for snowflake_num, snowflake_parameter in _snowflakes.items():
+        if snowflake_parameter['y'] < 0:
+            num_remove_snowflake.append(snowflake_num)
+
+    return True if len(num_remove_snowflake) != 0 else False
