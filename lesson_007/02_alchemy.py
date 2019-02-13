81e93c25e3c94dfff1928ef74033731afe58f874
zaboevai <zaboevai@yandex.ru>
2019-02-13 14:47:53 +0300

    les7, 02 - Прототип игры "Алхимия"

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,7 +19,213 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+
+class Water:
+
+    def __init__(self):
+        self.name = 'Water'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if other == Air():
+            substance = Storm()
+        elif other == Fire():
+            substance = Steam()
+        elif other == Earth():
+            substance = Dirt()
+        elif other == Iron():
+            substance = Rust()
+        else:
+            substance = None
+
+        return substance
+
+
+class Fire:
+
+    def __init__(self):
+        self.name = 'Fire'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if other == Air():
+            substance = Lightning()
+        elif other == Water():
+            substance = Steam()
+        elif other == Earth():
+            substance = Lava()
+        elif other == Iron():
+            substance = Ingot()
+        else:
+            substance = None
+
+        return substance
+
+
+class Air:
+
+    def __init__(self):
+        self.name = 'Air'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+
+        if other == Water():
+            substance = Dust()
+        elif other == Fire():
+            substance = Lightning()
+        elif other == Earth():
+            substance = Dust()
+        else:
+            substance = None
+
+        return substance
+
+
+class Earth:
+    def __init__(self):
+        self.name = 'Earth'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if other == Water():
+            substance = Dirt()
+        elif other == Fire():
+            substance = Lava()
+        elif other == Air():
+            substance = Dust()
+        elif other == Iron():
+            substance = Ore()
+        else:
+            substance = None
+
+        return substance
+
+
+class Steam:
+    name = 'Steam'
+
+    def __str__(self):
+        return self.name
+
+
+class Dirt:
+    name = 'Dirt'
+
+    def __str__(self):
+        return self.name
+
+
+class Storm:
+    name = 'Storm'
+
+    def __str__(self):
+        return self.name
+
+
+class Lightning:
+    name = 'Lightning'
+
+    def __str__(self):
+        return self.name
+
+
+class Dust:
+    name = 'Dust'
+
+    def __str__(self):
+        return self.name
+
+
+class Lava:
+    name = 'Lava'
+
+    def __str__(self):
+        return self.name
+
+
+class Iron:
+    def __init__(self):
+        self.name = 'Iron'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if other == Water():
+            substance = Rust()
+        elif other == Fire():
+            substance = Ingot()
+        elif other == Earth():
+            substance = Ore()
+        else:
+            substance = None
+
+        return substance
+
+
+class Rust:
+    """ржавчина"""
+    name = 'Rust'
+
+    def __str__(self):
+        return self.name
+
+
+class Ingot:
+    """слиток"""
+    name = 'Ingot'
+
+    def __str__(self):
+        return self.name
+
+
+class Ore:
+    """руда"""
+    name = 'Ore'
+
+    def __str__(self):
+        return self.name
+
+
+print(Water(), '+', Air(), '=', Water() + Air())
+print(Water(), '+', Fire(), '=', Water() + Fire())
+print(Water(), '+', Earth(), '=', Water() + Earth())
+print(Air(), '+', Fire(), '=', Air() + Fire())
+print(Air(), '+', Earth(), '=', Air() + Earth())
+print(Fire(), '+', Earth(), '=', Fire() + Earth())
+
+print('=====')
+print(Fire(), '+', Dust(), '=', Fire() + Dust())
+print(Earth(), '+', Fire(), '=', Earth() + Fire())
+
+print('=====')
+print(Iron(), '+', Fire(), '=', Iron() + Fire())
+print(Iron(), '+', Water(), '=', Iron() + Water())
+print(Iron(), '+', Earth(), '=', Iron() + Earth())
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
