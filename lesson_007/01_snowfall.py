f4738c26aae89d0563098b890a1137a4b274a8a6
zaboevai <zaboevai@yandex.ru>
2019-03-04 15:56:33 +0300

    les7, 01 - Переосмыслил всю процедуру снегопада, исправил согласно замечаний

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
 #  - отработку изменений координат
@@ -9,76 +10,56 @@ import simple_draw as sd
 
 
 class Snowflake:
-    snowflake_size = {'min': 5, 'max': 20}
-    snowflakes = {}
-    down_snowflakes = []
     y = 600
+    snowflake_size = {'min': 5, 'max': 20}
 
-    def __init__(self, count=1):
-        self.create_snowflakes(count)
+    def __init__(self):
+        self.parameter = self.__create_snowflake__()
 
-    def create_snowflakes(self, snowflakes_count=1):
+    def draw(self, color=sd.COLOR_WHITE):
+        start_point = sd.get_point(x=self.parameter['x'], y=self.parameter['y'])
+        sd.snowflake(center=start_point,
+                     length=self.parameter['length'],
+                     color=color,
+                     factor_a=self.parameter['factor_a'],
+                     factor_b=self.parameter['factor_b'],
+                     factor_c=self.parameter['factor_c'])
 
-        for i in range(snowflakes_count):
-            self.snowflakes[i] = {
-                'length': sd.random_number(self.snowflake_size['min'], self.snowflake_size['max']),
-                'x': sd.random_number(0, sd.resolution[0]),
-                'y': self.y,
-                'factor_a': sd.random_number(1, 10) / 10,
-                'factor_b': sd.random_number(1, 10) / 10,
-                'factor_c': sd.random_number(1, 120)
-                }
-
-    def clear_previous_picture(self, color=sd.background_color):
+    def clear(self, color=sd.background_color):
         self.draw(color)
 
     def move(self):
-        for snowflake_num, snowflake_parameter in self.snowflakes.items():
-            snowflake_parameter['x'] += sd.random_number(0, 2)
-            snowflake_parameter['y'] -= self.snowflake_size['max'] + 1 - snowflake_parameter['length']
+        self.parameter['x'] += sd.random_number(0, 2)
+        self.parameter['y'] -= self.snowflake_size['max'] + 1 - self.parameter['length']
 
-    def draw(self, color=sd.COLOR_WHITE):
-        for snowflake_num, snowflake_parameter in self.snowflakes.items():
-            start_point = sd.get_point(x=snowflake_parameter['x'], y=snowflake_parameter['y'])
-            sd.snowflake(center=start_point,
-                         length=snowflake_parameter['length'],
-                         color=color,
-                         factor_a=snowflake_parameter['factor_a'],
-                         factor_b=snowflake_parameter['factor_b'],
-                         factor_c=snowflake_parameter['factor_c'])
-
-    def can_fall(self):
-        for snowflake_num, snowflake_parameter in self.snowflakes.items():
-            if snowflake_parameter['y'] <= 0:
-                self.down_snowflakes.append(snowflake_num)
-        return self.down_snowflakes
-
-    def append_snowflakes(self, fallen_flakes):
-        for num in fallen_flakes:
-            self.snowflakes[num] = {
-                                    'length': sd.random_number(self.snowflake_size['min'], self.snowflake_size['max']),
-                                    'x': sd.random_number(0, sd.resolution[0]),
-                                    'y': self.y,
-                                    'factor_a': sd.random_number(1, 10) / 10,
-                                    'factor_b': sd.random_number(1, 10) / 10,
-                                    'factor_c': sd.random_number(1, 120)
-                                    }
-        self.down_snowflakes.clear()
+    def __create_snowflake__(self):
+        return {
+            'length': sd.random_number(self.snowflake_size['min'], self.snowflake_size['max']),
+            'x': sd.random_number(0, sd.resolution[0]),
+            'y': self.y,
+            'factor_a': sd.random_number(1, 10) / 10,
+            'factor_b': sd.random_number(1, 10) / 10,
+            'factor_c': sd.random_number(1, 120)
+        }
 
 
-flake = Snowflake(count=10)
+snowflakes = {}
+snowflakes_count = 10
+
+for i in range(snowflakes_count):
+    flake = Snowflake()
+    snowflakes[i] = flake
 
 while True:
     sd.start_drawing()
 
-    flake.clear_previous_picture()
-    flake.move()
-    flake.draw()
+    for num, snowflake in snowflakes.items():
+        snowflake.clear()
+        snowflake.move()
+        snowflake.draw()
 
-    falling_snowflakes = flake.can_fall()
-
-    if falling_snowflakes:
-        flake.append_snowflakes(falling_snowflakes)
+        if snowflake.parameter['y'] < 0:
+            snowflakes[num] = Snowflake()
 
     sd.sleep(0.05)
     sd.finish_drawing()
