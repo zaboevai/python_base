ff64bef3c3cdccd9a3c602ea601dd3785c7dc577
zaboevai <zaboevai@yandex.ru>
2019-03-28 11:11:04 +0300

    les9, 01 - Сделал подсчет ко-ва символов на основе классов

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -3,6 +3,7 @@ import zipfile
 import os
 import pprint
 
+
 # Подсчитать статистику по буквам в романе Война и Мир.
 # Входные параметры: файл для сканирования
 # Статистику считать только для букв алфавита (см функцию .isalpha() для строк)
@@ -21,40 +22,72 @@ import pprint
 # |  итого  | 9999999  |
 # +---------+----------+
 # Ширину таблицы подберите по своему вкусу
-src_dir = './python_snippets'
-unzip_src_dir = os.path.join(src_dir, 'unzip_files')
-file_name = 'voyna-i-mir.txt'
 
-for dir_path, dir_names, file_names in os.walk(src_dir):
-    for file_name in file_names:
-        if zipfile.is_zipfile(os.path.join(dir_path, file_name)):
-            src_file = os.path.join(dir_path, file_name)
-            print(src_file)
-            zip_file = zipfile.ZipFile(src_file)
-            zip_file.extractall(unzip_src_dir)
+class SymbolStat:
 
-symbols = {}
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.dir_path = os.path.dirname(self.file_path)
+        self.file_name = None
+        self.stat = {}
+        self.sum_symbols = 0
+        print(file_path)
+        if zipfile.is_zipfile(file_path):
+            self.unzip()
+        else:
+            self.file_name = os.path.basename(file_path)
 
-a, b = 'буква', 'кол-во'
-c = None
-print(f'')
-print('+',  f'{chr(45):-^9}',  '+',  f'{chr(45):-^10}',  '+', sep='')
-print('|',  f'{a: ^9}'      ,  '|',  f'{b: ^10}'      ,  '|', sep='')
-print('+',  f'{chr(45):-^9}',  '+',  f'{chr(45):-^10}',  '+', sep='')
+    def print_header(self):
+        header = ('буква', 'кол-во')
+        print(f'')
+        print('+', f'{chr(45):-^9}', '+', f'{chr(45):-^10}', '+', sep='')
+        print('|', f'{header[0]: ^9}', '|', f'{header[1]: ^10}', '|', sep='')
+        print('+', f'{chr(45):-^9}', '+', f'{chr(45):-^10}', '+', sep='')
 
-with open(os.path.join(unzip_src_dir, file_name), mode='r', encoding='CP1251') as file: #
-    for line in file:
-        for char in line:
-            if ord(char) in symbols:
-                symbols[ord(char)] += 1
+    def print_result(self):
+        res = 'Итого'
+        print('+', f'{chr(45):-^9}', '+', f'{chr(45):-^10}', '+', sep='')
+        print('|', f'{res: ^9}', '|', f'{self.sum_symbols: ^10}', '|', sep='')
+        print('+', f'{chr(45):-^9}', '+', f'{chr(45):-^10}', '+', sep='')
+
+    def print_body(self):
+        if not self.stat:
+            self.get_stat()
+
+        for char, cnt in sorted(self.stat.items()):
+            if char == 10:
+                symbol = 'enter'
             else:
-                symbols[ord(char)] = 1
-    # print(sorted(symbols.items()))
-    # symbols['перенос'] = symbols.pop(10)
-    # TODO подумать как правильно выводить chr(10)
-    for char, cnt in sorted(symbols.items()):
+                symbol = chr(char)
+            print('|', f'{symbol: ^9}', '|', f'{cnt: ^10}', '|', sep='')
 
-        print(f'|{chr(char): ^9}', '|', f'{cnt: ^10}|', sep='')
-        # break
-print(f'+{chr(45):-^9}', '+', f'{chr(45):-^10}+', sep='')
-# print(os.path.abspath('./'))
+    def print_stat(self):
+        self.print_header()
+        self.print_body()
+        self.print_result()
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_path, 'r')
+        for filename in zfile.namelist():
+            zfile.extractall(self.dir_path)
+            self.file_name = filename
+
+    def get_stat(self):
+        with open(os.path.join(self.dir_path, self.file_name), mode='r', encoding='CP1251') as file:
+
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        if ord(char) in self.stat:
+                            self.stat[ord(char)] += 1
+                        else:
+                            self.stat[ord(char)] = 1
+                        self.sum_symbols += 1
+
+        return self.stat
+
+
+if __name__ == '__main__':
+    src_file_path = './python_snippets/voyna-i-mir.txt.zip'
+    war_and_peace = SymbolStat(file_path=src_file_path)
+    war_and_peace.print_stat()
