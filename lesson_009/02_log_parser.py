8376770b962d21e34c117b3154c85bc0fbe8c4fb
zaboevai <zaboevai@yandex.ru>
2019-03-28 17:42:19 +0300

    les9, 02 - Попытка разбора файла с логом

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import zipfile, os, datetime
+
 # Имеется файл events.txt вида:
 #
 # [2018-05-17 01:55:52.665804] NOK
@@ -18,4 +20,36 @@
 #
 # Входные параметры: файл для анализа, файл результата
 
-# TODO здесь ваш код
+
+class LogParser:
+
+    def __init__(self, file_inc=None, file_out=None):
+        self.file_inc = file_inc
+        if file_out:
+            self.file_out = file_out
+        else:
+            self.file_out = file_inc + '.analyzed'
+        print(self.file_out)
+        self.stat = {}
+
+
+    def line_parsing(self, line=None):
+        date, res = line.split(']')
+        date = date.replace('[', '')
+        date, time = date.split(' ')
+        dt = []
+        dt.extend(date.split('-'))
+        dt.extend(time.split(':'))
+        sec = dt[-1].split('.')
+        dt.pop()
+        dt.extend(sec)
+        dt = tuple(map(int, dt))
+        dt = datetime.datetime(*dt)
+        return dt, res
+
+    def get_stat(self):
+        for line in self.file_inc:
+            dt, res = self.line_parsing(line)
+            if dt.year in self.stat
+if __name__ == '__main__':
+    test = LogParser('lesson_009/events.txt')
