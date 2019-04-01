281106cf525eca4751cf80ffa47a0c5687a93153
zaboevai <zaboevai@yandex.ru>
2019-04-01 23:22:16 +0300

    les9, 03 - Реализовал сортировщик файлов

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -2,6 +2,7 @@
 
 import os, time, shutil
 
+
 # Нужно написать скрипт для упорядочивания фотографий (вообще любых файлов)
 # Скрипт должен разложить файлы из одной папки по годам и месяцам в другую.
 # Например, так:
@@ -33,7 +34,57 @@ import os, time, shutil
 #
 # Чтение документации/гугла по функциям - приветствуется. Как и поиск альтернативных вариантов :)
 
-# TODO здесь ваш код
+class FileSorter:
+    """
+    Программа для сортировки файлов из каталога источника(src) в каталог назначения(dst).
+    Сортировка происходит по дате модификации файла в виде "/dst/year/month"
+    Сортировка происходит с учетом подкаталогов.
+    """
+    def __init__(self, src=None, dst=None):
+        self.src = os.path.normpath(src) if src else None
+        self.dst = os.path.normpath(dst) if dst else None
+
+    def get_dirs(self, mtime=None):
+        file_mdate = time.gmtime(mtime)
+        year, mon = str(file_mdate.tm_year), str(file_mdate.tm_mon)
+        file_dest = os.path.join(self.dst, *(year, mon if len(mon) != 1 else '0' + mon))
+        return file_dest
+
+    def make_dirs(self, dir=None):
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+    def copy_file(self, src=None, dst=None):
+        if not os.path.exists(dst):
+            shutil.copy2(src=src, dst=dst)
+
+    def arrange(self):
+        if not self.src:
+            print('Не указан каталог источник.')
+        elif not self.dst:
+            print('Не указан каталог назначения.')
+        else:
+            for dirPath, dirNames, fileNames in os.walk(self.src):
+                for file in fileNames:
+                    file_mtime = os.path.getmtime(os.path.join(dirPath, file))
+                    file_dst = self.get_dirs(file_mtime)
+                    self.make_dirs(file_dst)
+
+                    file_dst = os.path.join(file_dst, file)
+                    file_src = os.path.join(dirPath, file)
+                    self.copy_file(src=file_src, dst=file_dst)
+            if os.path.exists(self.dst):
+                print('Сортировка завершена успешно.', self.dst)
+            else:
+                print('Ошибка! Проверьте пути к каталогам.', self.src, self.dst)
+
+
+if __name__ == '__main__':
+    src_dir = '/home/andrey/Pictures'
+    dst_dir = '/home/andrey/Pictures/sorted'
+
+    files = FileSorter(src=src_dir, dst=dst_dir)
+    files.arrange()
 
 # Усложненное задание (делать по желанию)
 # Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
