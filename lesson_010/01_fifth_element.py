5c36de53264b1d47d668b191f48dff0c02669ceb
zaboevai <zaboevai@yandex.ru>
2019-04-15 16:58:06 +0300

    les10, 01 - Добавил обработку исключений

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -3,11 +3,19 @@
 # Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
 
 BRUCE_WILLIS = 42
+input_data = None
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+try:
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError as exc:
+    print(f'Введено: <{input_data}> - Невозможно преобразовать к числу ({exc}).')
+except IndexError as exc:
+    print(f'Введено: <{input_data}> - Выход за границы списка ({exc}).')
+except:
+    print(f'Введено: <{input_data}> - Непредвиденная ошибка.')
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
