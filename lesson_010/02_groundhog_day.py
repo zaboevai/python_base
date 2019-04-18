8cff3df25758effeaa7e4ca9d6c21a69bfb2cdb2
zaboevai <zaboevai@yandex.ru>
2019-04-18 09:01:53 +0300

    les10, 02 - смоделировал "День сурка"

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import random
 # День сурка
 #
 # Напишите функцию one_day() которая возвращает количество кармы от 1 до 7
@@ -16,7 +16,57 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
+carma = 0
 
-# TODO здесь ваш код
+
+class IamGodError(Exception):
+    pass
+
+
+class DrunkError(Exception):
+    pass
+
+
+class CarCrashError(Exception):
+    pass
+
+
+class GluttonyError(Exception):
+    pass
+
+
+class DepressionError(Exception):
+    pass
+
+
+class SuicideError(Exception):
+    pass
+
+
+def one_day():
+    my_exceptions = (IamGodError('IamGod - Карма не растет'),
+                     DrunkError('Drunk - Карма не растет'),
+                     CarCrashError('CarCrash - Карма не растет'),
+                     GluttonyError('Gluttony - Карма не растет') ,
+                     DepressionError('Depression - Карма не растет'),
+                     SuicideError('Suicide - Карма не растет'),)
+
+    if random.randint(1, 13) == 13:
+        raise random.choice(my_exceptions)
+    return random.randint(1, 7)
+
+
+with open('02_error.log', mode='w') as file:
+    while True:
+        try:
+            carma += one_day()
+            if carma >= ENLIGHTENMENT_CARMA_LEVEL:
+                print(carma)
+                break
+        except (IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError) as exc:
+            error = f'Ошибка: {exc.__class__.__name__} {exc.args}'
+            print(error)
+            file.write(error)
+            file.write('\n')
 
 # https://goo.gl/JnsDqu
