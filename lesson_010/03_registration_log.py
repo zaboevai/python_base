31ccf0887e75059b66d5b4156da0ebeb638f751e
zaboevai <zaboevai@yandex.ru>
2019-04-24 10:06:57 +0300

    les10, 01,03 - исправил код согласно замечаний

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -32,45 +32,45 @@ class NotEmailError(Exception):
     pass
 
 
+def is_email(email):
+    return '@' in email and '.' in email
+
+
+def check_age_limit(age):
+    return 10 <= int(age) <= 99
+
+
 def check_line_log(line):
     name, email, age = line.split(' ')
-    print(line, '1' if name and email and age else '0')
-    # TODO В данной проверке нет необходимости, потому если строка не будет
-    # TODO содержать всех необходимых данных, то интерпретатор сам возбудит
-    # TODO исключение в строке 36
-    if name and email and age:
-        # TODO Проверки лучше вынесли в отдельные функции
-        if name.isalpha():
-            if '@' in email and '.' in email:
-                if 10 <= int(age) <= 99:
-                    return f'{name:<10} {email:<30} {age}\n'
-                else:
-                    raise ValueError('поле возраст НЕ является числом от 10 до 99')
+
+    if name.isalpha():
+        if is_email(email):
+            if check_age_limit(age):
+                return f'{name:<10} {email:<30} {age}\n'
             else:
-                raise NotEmailError('поле емейл НЕ содержит @ и .(точку)')
+                raise ValueError('поле возраст НЕ является числом от 10 до 99')
         else:
-            raise NotNameError('поле имени содержит НЕ только буквы')
+            raise NotEmailError('поле емейл НЕ содержит @ и .(точку)')
     else:
-        raise ValueError('НЕ присутсвуют все три поля')
+        raise NotNameError('поле имени содержит НЕ только буквы')
 
 
-with open('registrations.txt', mode='r', encoding='utf8') as file:
-    with open('registrations_good.log', mode='w', encoding='utf8') as good_log:
-        with open('registrations_bad.log', mode='w', encoding='utf8') as bad_log:
+log_path = 'registrations.txt'
+bad_log_path = 'registrations_bad.log'
+good_log_path = 'registrations_good.log'
+
+with open(log_path, mode='r', encoding='utf8') as file:
+    with open(good_log_path, mode='w', encoding='utf8') as good_log:
+        with open(bad_log_path, mode='w', encoding='utf8') as bad_log:
             for file_line in file:
                 line_cont += 1
                 line = file_line[:-1].strip()
                 try:
                     good_line = check_line_log(line)
-                    good_log.write(good_line)
-                # TODO Обработка всех исключений практически идентична, лучше
-                # TODO будет в одном except перехватывать несколько исключений
-                except ValueError as exc:
+                except BaseException as exc:
                     if 'unpack' in exc.args[0]:
                         bad_log.write(f'{line_cont:<5} {line:<40} НЕ присутсвуют все три поля\n')
                     else:
                         bad_log.write(f'{line_cont:<5} {line:<40} {exc}\n')
-                except NotEmailError as exc:
-                    bad_log.write(f'{line_cont:<5} {line:<40} {exc}\n')
-                except NotNameError as exc:
-                    bad_log.write(f'{line_cont:<5} {line:<40} {exc}\n')
+                else:
+                    good_log.write(good_line)
