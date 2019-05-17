419518becc8054017cfdad5258a973c5e9bc00f8
zaboevai <zaboevai@yandex.ru>
2019-05-17 16:20:28 +0300

    les11, 01 - Исправил согласно замечаний

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -14,36 +14,6 @@ from lesson_004 import shapes
 #
 # Функция-фабрика должна принимать параметр n - количество сторон.
 
-class NotExpectedPolygon(Exception):
-    pass
-
-
-def get_polygon(n):
-    # TODO Необходимо реализовать замыкание, как это сделать показано в
-    # TODO "11.10 Практика - функциональный стиль. Часть 1"
-    if n == 3:
-        # TODO Необходимо использовать универсальную функцию, а не функции
-        # TODO основанные на ней.
-        # TODO Импортировать функции из урока 4 не нужно
-        return shapes.draw_triangle
-    elif n == 4:
-        return shapes.draw_quadrate
-    elif n == 5:
-        return shapes.draw_pentagon
-    elif n == 6:
-        return shapes.draw_hexagon
-    else:
-        # TODO В данном случае не нужно ограничивать пользователя в выборе
-        # TODO фигуры, по этому исключения это лишнее
-        raise NotExpectedPolygon(
-            f'Ошибка в <{get_polygon.__name__}>: разрешено кол-во сторон для ввода n = (3, 4, 5, 6), введено = <{n}>')
-
-
-try:
-    draw_figure = get_polygon(n=5)
-    draw_figure(start_point=sd.get_point(150, 150), angle=13, length=200)
-except NotExpectedPolygon as exc:
-    print(exc)
 
 def create_polygon(n):
 
