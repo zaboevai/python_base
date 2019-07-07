f78210384b6ba55d4c903a48b39f5385302c171d
zaboevai <zaboevai@yandex.ru>
2019-07-07 23:25:50 +0300

    les13, 01 - Создал пакет по заполнению данных полета

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+import argparse
 
 # Заполнить все поля в билете на самолет.
 # Создать функцию, принимающую параметры: ФИО, откуда, куда, дата вылета,
@@ -8,9 +11,58 @@
 # Пример заполнения lesson_013/images/ticket_sample.png
 # Подходящий шрифт искать на сайте ofont.ru
 
-def make_ticket(fio, from_, to, date):
-    # TODO здесь ваш код
-    pass
+COLOR_WHITE = ImageColor.getrgb('white')
+COLOR_BLACK = ImageColor.getrgb('black')
+PARENT_DIR = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_FONT = os.path.join(PARENT_DIR, 'font', 'ofont.ru_Franklin Gothic Medium Cond.ttf')
+
+FIO_COORD = (45, 130)
+FLY_FROM_COORD = (45, 200)
+FLY_TO_COORD = (45, 265)
+DATE_COORD = (285, 265)
+
+
+def prepare_template(draw: ImageDraw):
+    draw.rectangle([FIO_COORD, (250, 160)], fill=COLOR_WHITE, width=1)
+    draw.rectangle([FLY_FROM_COORD, (250, 230)], fill=COLOR_WHITE, width=1)
+    draw.rectangle([FLY_TO_COORD, (350, 295)], fill=COLOR_WHITE, width=1)
+
+
+def make_ticket(fio=None, from_=None, to_=None, date=None, dest=None):
+    # TODO дополнить функцию проверками
+    template_path = os.path.join(PARENT_DIR, 'images', 'ticket_template.png')
+
+    img = Image.open(template_path)
+    draw = ImageDraw.Draw(im=img)
+
+    prepare_template(draw)
+
+    font = ImageFont.truetype(font=DEFAULT_FONT, size=18, )
+    fio = fio.upper()
+    from_ = from_.upper()
+    to = to_.upper()
+    date = date.upper()
+
+    draw.text(xy=FIO_COORD, text=fio, fill=COLOR_BLACK, font=font)
+    draw.text(xy=FLY_FROM_COORD, text=from_, fill=COLOR_BLACK, font=font)
+    draw.text(xy=FLY_TO_COORD, text=to, fill=COLOR_BLACK, font=font)
+    draw.text(xy=DATE_COORD, text=date, fill=COLOR_BLACK, font=font)
+
+    if dest and not os.path.exists(dest):
+        try:
+            os.mkdir(os.path.join(dest))
+            dest_path = os.path.join(dest, 'test1.png')
+        except PermissionError:
+            print('ОШИБКА: Нет прав на запись!')
+            return False
+
+    else:
+        dest_path = os.path.join(PARENT_DIR, dest, 'test1.png')
+
+    print(dest_path)
+    img.save(dest_path, 'PNG')
+    print(f'Создан билет на основе макета в каталоге {dest_path}')
+    return True
 
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля agrparse.
@@ -21,3 +73,22 @@ def make_ticket(fio, from_, to, date):
 #   --date - обязательный, когда летим.
 #   --save_to - необязательный, путь для сохранения заполненнего билета.
 # и заполнять билет.
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Утилита для формирования АВИА билета перевозчика "SKILLBOX AIRLINE".',
+                                     add_help=True)
+
+    parser.add_argument('fio', type=str, help='ФИО')
+    parser.add_argument('from_', type=str, help='Откуда')
+    parser.add_argument('to_', type=str, help='Куда')
+    parser.add_argument('date', type=str, help='Дата вылета')
+    parser.add_argument('-dst', '--destination', type=str, default='', help='Путь к заполненному файлу (default: '')')
+    args = parser.parse_args()
+    print(args, args.destination)
+
+    if args:
+        make_ticket(args.fio, args.from_, args.to_, args.date, args.destination)
+
+# make_ticket(fio='Забоев Андрей Игоревич', from_='Киров', to_='Сочи', date='04.07')
