c1ab2dd4c69c96ff3f5f3e2d469f314303c66ced
zaboevai <zaboevai@yandex.ru>
2019-08-11 23:23:42 +0300

    les14, курсовая работа - добавил 2 теста на функции бота

--- chatbot/tests/test_vk_bot.py
+++ chatbot/tests/test_vk_bot.py
@@ -1,39 +1,50 @@
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, ANY
 
-import main
+from vk_api.bot_longpoll import VkBotMessageEvent
+
 from main import Bot
 
 
-class Test1(unittest.TestCase):
+class TestBot(unittest.TestCase):
+    RAW_EVENT = {'type': 'message_new',
+                 'object': {'date': 1565551500, 'from_id': 4145622, 'id': 194, 'out': 0, 'peer_id': 4145622,
+                            'text': 'as', 'conversation_message_id': 194, 'fwd_messages': [], 'important': False,
+                            'random_id': 0, 'attachments': [], 'is_hidden': False},
+                 'group_id': 184332451}
 
     def test_bot_start(self):
         count = 5
         obj = [{'a': 1}]
         events = [obj] * count
 
-        # session = Mock()
-        # vk_api_method = Mock()
-        # session.get_api = vk_api_method
-        # vk = session.get_api('')
-
         long_poller_mock = Mock(return_value=events)
-        VkLongPoll = Mock()
-        long_poller_listen_mock = VkLongPoll
+        long_poller_listen_mock = Mock()
         long_poller_listen_mock.listen = long_poller_mock
 
-        # bot = Bot('', '')
-        # bot.on_event = Mock()
-        # bot.start()
-
-        # self.assertEquals().
-
         with patch('main.vk_api.VkApi'):
-            with patch('main.VkLongPoll', return_value=long_poller_listen_mock):
+            with patch('main.VkBotLongPoll', return_value=long_poller_listen_mock):
                 bot = Bot('', '')
                 bot.on_event = Mock()
                 bot.start()
 
                 bot.on_event.assert_called()
                 bot.on_event.assert_any_call(obj)
-                assert bot.on_event.call_count == 5
+                assert bot.on_event.call_count == count
+
+    def test_on_event(self):
+        event = VkBotMessageEvent(raw=self.RAW_EVENT)
+
+        send_mock = Mock()
+
+        with patch('main.vk_api.VkApi'):
+            with patch('main.VkBotLongPoll'):
+                bot = Bot('', '')
+                bot.api = Mock()
+                bot.api.messages.send = send_mock
+
+                bot.on_event(event)
+
+        send_mock.assert_called_once_with(message=self.RAW_EVENT['object']['text'],
+                                          random_id=ANY,
+                                          peer_id=self.RAW_EVENT['object']['peer_id'])
